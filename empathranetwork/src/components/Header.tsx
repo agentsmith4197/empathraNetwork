233c1d51092ec4f10a7f4a18bdc6a0878b528832
agentsmith4197 <smithmandu@gmail.com>
2026-03-04 09:04:44 -0800

    updates

--- empathranetwork/src/components/Header.tsx
+++ empathranetwork/src/components/Header.tsx
@@ -6,14 +6,28 @@ const Header = () => {
     <header className="header">
       <div className="header-container">
 
-        <h1 className="logo">
-          Empathra <span>Network</span>
-        </h1>
+        {/* LOGO */}
+
+        <Link to="/" className="logo">
+          <img
+            src="/logo.png"
+            alt="Empathra Network Logo"
+            className="logo-image"
+          />
+
+          <span className="logo-text">
+            Empathra <strong>Network</strong>
+          </span>
+        </Link>
+
+        {/* NAVIGATION */}
 
         <nav className="nav-links">
           <Link to="/">Home</Link>
           <Link to="/create-campaign">Start Campaign</Link>
         </nav>
+
+        {/* AUTH BUTTONS */}
 
         <div className="auth-buttons">
           <button className="login">Login</button>
